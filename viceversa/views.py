2c923cba5634657e257d6eba5d0bc4202454bce5
Mbarov <prom@tgv73.ru>
2021-05-05 17:15:32 +0500

    'kvo'

--- viceversa/views.py
+++ viceversa/views.py
@@ -8,4 +8,6 @@ def home(request):
 def reverse(request):
     user_text= request.GET['usertext']
     reversed_text= user_text[::-1]
-    return render(request,'reverse.html', {'usertext':user_text, 'reverse':reversed_text})
+    kvo=user_text.split()
+    kvo2=len(kvo)
+    return render(request,'reverse.html', {'usertext':user_text, 'reverse':reversed_text,'kvo':kvo2})
